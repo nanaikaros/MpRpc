ca8c066de10972a51b4878309d610f8e3ed4e175
sortdev <sortdev@example.com>
2024-05-26 20:56:44 -0800

    Add

---
+++ build/CMakeFiles/mprpc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mprpc.dir/main.cpp.o"
+  "CMakeFiles/mprpc.dir/main.cpp.o.d"
+  "mprpc"
+  "mprpc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mprpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Project/MpRpc
+# Build directory: /Project/MpRpc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
